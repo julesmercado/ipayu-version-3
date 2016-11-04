9e615dfdcdcf6fd5229f627d560f50728d753a2f
Jiyu Win <akosijiyuwin@gmail.com>
2016-11-04 13:54:45 +0800

    designs

--- ipayu/www/angular/mall-info/controller/mallinfo.events.cards.controller.js
+++ ipayu/www/angular/mall-info/controller/mallinfo.events.cards.controller.js
@@ -66,7 +66,7 @@
                 $uibModal.open({
                       ariaLabelledBy: 'modal-title',
                       ariaDescribedBy: 'modal-body',
-                      templateUrl: '../templates/modals/comment-modal.html',
+                      templateUrl: '../www/templates/modals/comment-modal.html',
                       controller: 'modalComments'
                       
                     })
