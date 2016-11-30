39e8bda5e9f6811d929820d53a43a510b10b880a
changxiu1 <graham.jimenez95@gmail.com>
2016-11-30 11:53:51 +0800

    asd

--- ipayu/www/angular/mall-info/controller/mallinfo.shopinfo.controller.js
+++ ipayu/www/angular/mall-info/controller/mallinfo.shopinfo.controller.js
@@ -3,5 +3,11 @@
         function( $scope , mallData , $state ){ /*, offlineData*/
             
             $scope.shopInfo = mallData.setShopInfo();
+        
+            $scope.download_file = function(file){
+                console.log('download')
+                window.open(file, '_system');
+                
+            }
             
     } )
